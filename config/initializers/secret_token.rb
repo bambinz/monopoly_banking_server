ca9b1c830f7c6c33d770c4f666d343e9ae9cb776
Michael Baumberger <michael@moroku.com>
2014-02-15 11:23:06 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonopolyBanking::Application.config.secret_token = '5b88a69356cc643583fdaadecf8f82822022138f879631a65cf57c4e369017c83321235b9ef57a9a612f9fc92064c7461b5f47135a11aa0b2d94a4cb56b9f0e3'
